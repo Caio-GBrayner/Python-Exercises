9592d20b12d05e3ba92034b121f0c918bc3d690c
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-02-08 02:20:57 -0300

    Employee salary increase and Celsius °C to Fahrenheit °F

---
+++ 01-13/ex13.py
@@ -0,0 +1,2 @@
+salary = float(input("What is the employee's salary? R$"))
+print(f"An employee was who earned {salary:.2f} with a 15% increase, starts to receive {salary + salary *(15/100):.2f}")
---
+++ 01-14/ex14.py
@@ -0,0 +1,3 @@
+temperature = float(input("Inform the temperature in Cº"))
+
+print(f"The temperature of {temperature:.1f}Cº corresponds to {(temperature * (9/5) + 32)}ºF")
