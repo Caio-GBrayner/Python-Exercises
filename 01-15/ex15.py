e02beb1899bfef46abb855c873172d5dca897f6d
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-02-10 10:25:25 -0300

    Car rental

---
+++ 01-15/ex15.py
@@ -0,0 +1,5 @@
+km = float(input("How many kilometers traveled? "))
+time = float(input("How many day the car was used? "))
+formulaTotal = (km*0.15) + (time*60.00)
+
+print(f"The amount to be paid: R${formulaTotal} ")
